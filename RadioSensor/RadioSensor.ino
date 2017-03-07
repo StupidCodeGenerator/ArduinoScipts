09885cd0344ff570e4baa0c9ac0c2a3ed83097cc
stupidcodegenerator <c@thebgame.com>
2017-03-07 23:54:03 +0800

    update

---
+++ RadioSensor/RadioSensor.ino
@@ -0,0 +1,85 @@
+/*
+  Analog Input
+ Demonstrates analog input by reading an analog sensor on analog pin 0 and
+ turning on and off a light emitting diode(LED)  connected to digital pin 13.
+ The amount of time the LED will be on and off depends on
+ the value obtained by analogRead().
+
+ The circuit:
+ * Potentiometer attached to analog input 0
+ * center pin of the potentiometer to the analog pin
+ * one side pin (either one) to ground
+ * the other side pin to +5V
+ * LED anode (long leg) attached to digital output 13
+ * LED cathode (short leg) attached to ground
+
+ * Note: because most Arduinos have a built-in LED attached
+ to pin 13 on the board, the LED is optional.
+
+
+ Created by David Cuartielles
+ modified 30 Aug 2011
+ By Tom Igoe
+
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/AnalogInput
+
+ */
+
+int sensorPin = A1;    // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+  if(sensorValue == 0){
+  	digitalWrite(2, LOW);
+  	digitalWrite(3, LOW);
+  	digitalWrite(4, LOW);
+  	digitalWrite(5, LOW);
+  	digitalWrite(6, LOW);
+  } else if (sensorValue < 10){
+  	digitalWrite(2, LOW);
+  	digitalWrite(3, LOW);
+  	digitalWrite(4, LOW);
+  	digitalWrite(5, LOW);
+  	digitalWrite(6, HIGH);
+  } else if (sensorValue < 20){
+  	digitalWrite(2, LOW);
+  	digitalWrite(3, LOW);
+  	digitalWrite(4, LOW);
+  	digitalWrite(5, HIGH);
+  	digitalWrite(6, HIGH);
+  } else if (sensorValue < 30){
+	digitalWrite(2, LOW);
+  	digitalWrite(3, LOW);
+  	digitalWrite(4, HIGH);
+  	digitalWrite(5, HIGH);
+  	digitalWrite(6, HIGH);
+  } else if (sensorValue < 40){
+  	digitalWrite(2, LOW);
+  	digitalWrite(3, HIGH);
+  	digitalWrite(4, HIGH);
+  	digitalWrite(5, HIGH);
+  	digitalWrite(6, HIGH);
+  } else {
+	digitalWrite(2, HIGH);
+  	digitalWrite(3, HIGH);
+  	digitalWrite(4, HIGH);
+  	digitalWrite(5, HIGH);
+  	digitalWrite(6, HIGH);
+  }
+}
